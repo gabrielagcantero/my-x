bd4ddb157eb06f8c76ed23e37a26b92587163f8f
gabrielagcantero <gabrielagcantero@gmail.com>
2023-12-23 20:25:54 -0300

    Router & Showing posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,9 @@ import { SignIn, SignOutButton, useUser, SignInButton } from "@clerk/nextjs";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.post.hello.useQuery({ text: "from tRPC" });
-
   const user = useUser();
+
+  const { data } = api.post.getAll.useQuery();
 
   return (
     <>
@@ -16,10 +16,14 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div>
-          { user? <SignOutButton/> : <SignInButton/> }
-        </div>
+        { user? <SignOutButton/> : <SignInButton/> }
         <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
